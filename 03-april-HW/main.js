cb8ab6ef5d9364346fa099bc56f2335d45921296
ElmarNi <46805869+ElmarNi@users.noreply.github.com>
2019-04-03 22:12:57 +0400

    Add files via upload

---
+++ 03-april-HW/main.js
@@ -0,0 +1,27 @@
+"use strict"
+
+const userInputTagName = document.querySelector(".input-tag-name");
+const mainDiv = document.querySelector(".main-div")
+userInputTagName.onkeydown = function(e)
+{
+    if (e.keyCode == 9) {
+        const tagName = document.createElement("div");
+        const icon = document.createElement("i");
+        const tagNameValue = userInputTagName.value;
+        icon.classList = "far fa-window-close ml-2";
+        tagName.classList = "tag-div d-flex align-items-center"
+        tagName.innerHTML = tagNameValue;
+        tagName.appendChild(icon)
+        icon.onclick = () => icon.parentNode.remove()
+        const el = [...mainDiv.children].find(n=> n.innerText == userInputTagName.value);
+        if(el !== undefined){
+            alert("This tag is already exsist ! But don't worry about it because we'll solve it");
+            el.remove();
+        }
+        mainDiv.insertBefore(tagName,userInputTagName.previousSibling)
+        userInputTagName.value = ""
+    }
+}
+
+
+

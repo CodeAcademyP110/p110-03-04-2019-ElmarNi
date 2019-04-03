5ad6f4fb722eec72a481150a8354f9cb8f35c8ad
ElmarNi <46805869+ElmarNi@users.noreply.github.com>
2019-04-03 22:32:22 +0400

    Add files via upload

--- 03-april-HW/main.js
+++ 03-april-HW/main.js
@@ -7,13 +7,16 @@ userInputTagName.onkeydown = function(e)
     if (e.keyCode == 9) {
         const tagName = document.createElement("div");
         const icon = document.createElement("i");
-        const tagNameValue = userInputTagName.value;
+        const tagNameValue = userInputTagName.value.trim();
         icon.classList = "far fa-window-close ml-2";
         tagName.classList = "tag-div d-flex align-items-center"
         tagName.innerHTML = tagNameValue;
+        if (tagNameValue === "") {
+            return
+        }
         tagName.appendChild(icon)
         icon.onclick = () => icon.parentNode.remove()
-        const el = [...mainDiv.children].find(n=> n.innerText == userInputTagName.value);
+        const el = [...mainDiv.children].find(n=> n.innerText == tagNameValue);
         if(el !== undefined){
             alert("This tag is already exsist ! But don't worry about it because we'll solve it");
             el.remove();
